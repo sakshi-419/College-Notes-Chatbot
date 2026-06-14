338f73557786653f68a8d740f2c16361f18ecc12
sakshi-419 <witness.code04@gmail.com>
2026-06-14 17:26:47 +0530

    Set Python 3.11

--- src/config.py
+++ src/config.py
@@ -1,30 +1,13 @@
-from dotenv import load_dotenv
-import streamlit as st
 import os
-
-from langchain_google_genai import (
-    ChatGoogleGenerativeAI,
-    GoogleGenerativeAIEmbeddings
-)
+import streamlit as st
+from dotenv import load_dotenv
 
 load_dotenv()
 
-GEMINI_API_KEY = os.getenv(
-    "GEMINI_API_KEY",
-    st.secrets.get("GEMINI_API_KEY", None)
-)
+if "GEMINI_API_KEY" in st.secrets:
+    GEMINI_API_KEY = st.secrets["GEMINI_API_KEY"]
+else:
+    GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 if not GEMINI_API_KEY:
     raise ValueError("GEMINI_API_KEY not found")
-
-embeddings = GoogleGenerativeAIEmbeddings(
-    model="models/embedding-001",
-    google_api_key=GEMINI_API_KEY
-)
-
-llm = ChatGoogleGenerativeAI(
-    model="gemini-1.5-flash",
-    google_api_key=GEMINI_API_KEY,
-    temperature=0.2
-)
-
