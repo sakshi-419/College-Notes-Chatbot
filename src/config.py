4290b37a0bdadb866fce1a9092f9bfdb3288a33f
sakshi-419 <witness.code04@gmail.com>
2026-06-14 12:24:55 +0530

    Support Streamlit secrets

--- src/config.py
+++ src/config.py
@@ -1,19 +1,10 @@
 from dotenv import load_dotenv
+import streamlit as st
 import os
-
-from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_google_genai import ChatGoogleGenerativeAI
 
 load_dotenv()
 
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-
-embeddings = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-MiniLM-L6-v2"
-)
-
-llm = ChatGoogleGenerativeAI(
-    model="gemini-2.5-flash",
-    google_api_key=GEMINI_API_KEY,
-    temperature=0.3
+GEMINI_API_KEY = os.getenv(
+    "GEMINI_API_KEY",
+    st.secrets.get("GEMINI_API_KEY", None)
 )
